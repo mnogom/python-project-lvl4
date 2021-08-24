9e7e5d151ae136c202f663e3cb052dce6fd2e846
Konstantin Freidlin <freidlinks@ya.ru>
2021-08-24 11:40:10 +0300

    Prepare for i18n

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ requirements:
 
 run:
 	poetry run python manage.py runserver
+
+lint:
+	poetry run flake8 task_manager
--- task_manager/views.py
+++ task_manager/views.py
@@ -4,7 +4,10 @@ from django.views import View
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.utils.translation import gettext
+
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
+        return HttpResponse(gettext("Hello"))
         return render(request, 'index.html', context={'env': os.getenv('ENV')})
