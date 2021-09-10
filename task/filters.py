911615410edf6c8a03889b66d9d55742b4d89365
Konstantin Freidlin <freidlinks@ya.ru>
2021-09-10 04:56:27 +0300

    Trying fix ':' at filter label

--- task/filters.py
+++ task/filters.py
@@ -5,18 +5,30 @@ from django import forms
 from django.utils.translation import gettext_lazy as _
 
 from .models import Task
+from user.models import User
 from label.models import Label
+from status.models import Status
 
 
 class TaskFilter(django_filters.FilterSet):
     """Task filter."""
-
-    self_tasks = django_filters.BooleanFilter(widget=forms.CheckboxInput,
-                                              label=_('Only my tasks'),
-                                              method='choose_user_as_author')
+    widget = ''
+    self_tasks = django_filters.BooleanFilter(label=_('Only my tasks'),
+                                              method='choose_user_as_author',
+                                              widget=forms.CheckboxInput(attrs={'id': 'id_self_tasks'}))
     label = django_filters.ModelChoiceFilter(queryset=Label.objects.all().order_by('pk'),
                                              label=_('Label'),
-                                             method='choose_label')
+                                             method='choose_label',
+                                             widget=forms.Select(attrs={'id': 'id_label'}))
+    author = django_filters.ModelChoiceFilter(queryset=User.objects.all().order_by('pk'),
+                                              label=_('Author'),
+                                              widget=forms.Select(attrs={'id': 'id_author'}))
+    executor = django_filters.ModelChoiceFilter(queryset=User.objects.all().order_by('pk'),
+                                                label=_('Executor'),
+                                                widget=forms.Select(attrs={'id': 'id_executor'}))
+    status = django_filters.ModelChoiceFilter(queryset=Status.objects.all().order_by('pk'),
+                                              label=_('Status'),
+                                              widget=forms.Select(attrs={'id': 'id_status'}))
 
     class Meta:
         model = Task
