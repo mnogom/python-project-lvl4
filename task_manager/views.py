033deb4480f00814f7750af7648248046ad1a57b
Konstantin Freidlin <freidlinks@ya.ru>
2021-09-15 20:01:56 +0300

    Add 'rollbar' test url

--- task_manager/views.py
+++ task_manager/views.py
@@ -3,7 +3,10 @@
 import os
 
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+
+import rollbar
 
 
 class HomeView(View):
@@ -11,5 +14,12 @@ class HomeView(View):
 
     def get(self, request, *args, **kwargs):
         """Method GET."""
+
+        if request.GET.get('rollbar-test'):
+            rollbar.report_message(message=f'This is test from '
+                                           f'"python-project-lvl4" '
+                                           f'with mode "{os.getenv("ENV")}"',
+                                   level="info")
+            return redirect(reverse_lazy('index'))
 
         return render(request, 'index.html', context={'env': os.getenv('ENV')})
